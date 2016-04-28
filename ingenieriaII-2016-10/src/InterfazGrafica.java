72e80f4bcef937fdc8e19e19709cba8e64f82d01
Heriberto Espino Martinez <hespinom@hotmail.com>
2016-04-28 19:52:47 +0200

    Crea la clase InterfazGrafica

---
+++ ingenieriaII-2016-10/src/InterfazGrafica.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class InterfazGrafica extends JFrame {
+	
+		private static final long serialVersionUID = 1L;
+
+	public InterfazGrafica() {
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	setSize(400, 500);
+	setTitle("Creado por Espino");
+	}
+
+}
+
