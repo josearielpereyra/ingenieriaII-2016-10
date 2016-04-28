c7306f54bcf3a895d6649c336bcc41789c647e5e
Estudiante <Estudiante@LabInf2_PC_12>
2016-04-28 13:41:49 -0400

    Actualizar barra de titulo

---
+++ ingenieriaII-2016-10/src/ventanas/VentanaDeWendy.java
@@ -0,0 +1,11 @@
+package ventanas;
+
+import javax.swing.JFrame;
+
+public class VentanaDeWendy extends JFrame{
+	public VentanaDeWendy(){
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(400, 300);
+		setTitle("Creado por Wendy");
+	}
+}
